0311869cdd450d583addac25d511b04f5006b8f6
pmaupin <pmaupin@267ec534-23b7-11df-a66d-ddd16ca3c27a>
2010-03-04 17:37:31 +0000

    Tidied up double-quoted string handling a bit

--- py2x/rson/doublequoted.py
+++ py2x/rson/doublequoted.py
@@ -7,6 +7,7 @@ See http://code.google.com/p/rson/source/browse/#svn/trunk/license.txt
 '''
 
 
+import sys
 import re
 
 class QuotedToken(object):
@@ -19,15 +20,15 @@ class QuotedToken(object):
     join = u''.join
     cachestrings = False
 
-    splitter = re.compile(r'(\\.)').split
-    mapper = { '\\\\' : '\\',
-               r'\"' : '"',
-               r'\/' : '/',
-               r'\b' : '\b',
-               r'\f' : '\f',
-               r'\n' : '\n',
-               r'\r' : '\r',
-               r'\t' : '\t'}.get
+    splitter = re.compile(r'(\\u[0-9a-fA-F]{4}|\\.)').split
+    mapper = { '\\\\' : u'\\',
+               r'\"' : u'"',
+               r'\/' : u'/',
+               r'\b' : u'\b',
+               r'\f' : u'\f',
+               r'\n' : u'\n',
+               r'\r' : u'\r',
+               r'\t' : u'\t'}.get
 
     @classmethod
     def factory(cls, int=int):
@@ -38,44 +39,53 @@ class QuotedToken(object):
         join = cls.join
         cachestrings = cls.cachestrings
 
+        allow_double = sys.maxunicode > 65535
+
         def parse(token):
             s = token[2]
             if len(s) < 2 or not (s[0] == s[-1] == '"'):
                 token[-1].error('No end quote on string', token)
             s = s[1:-1]
-            if '\\' not in s:
-                result = makestr(token, s)
-            else:
-                s = [mapper(x, x) for x in splitter(s) if x]
-                check = set(x for x in s if x.startswith('\\') and len(x) > 1)
-                if check:
-                    bad = [x for x in check if x != '\\u']
-                    if bad:
-                        token[-1].error('Invalid escaped string char %s' % bad[0], token)
-                    s.append('')
-                    s.reverse()
-                    next = s.pop
-                    result = []
-                    append = result.append
-                    while s:
-                        x = next()
-                        if x  != '\\u':
-                            append(makestr(token, x))
-                            continue
-                        x = next()
-                        leftovers = x[4:]
-                        digits = (x[:4] + 'ZZZZ')[:4]
-                        try:
-                            value = int(digits, 16)
-                        except:
-                            token[-1].error('Invalid escaped unicode char: \\u %s' % repr(x[:4]), token)
-                        append(makechr(value))
-                        append(makestr(token, leftovers))
-                else:
-                    result = [makestr(token, x) for x in s]
-                result = join(result)
+            result = []
+            append = result.append
+            s = iter(splitter(s))
+            next = s.next
+            append(makestr(token, next()))
+            for special in s:
+                nonmatch = next()
+                remap = mapper(special)
+                if remap is None:
+                    if special.startswith(r'\u') and len(special) == 6:
+                        uni = int(special[2:], 16)
+                        if 0xd800 <= uni <= 0xdbff and allow_double:
+                            uni, nonmatch = parse_double(uni, nonmatch, next, token)
+                        remap = makechr(uni)
+                    else:                    
+                        token[-1].error('Invalid character in quoted string: %s' % repr(special), token)
+                append(remap)
+                append(makestr(token, nonmatch))
+
+            result = join(result)
             if cachestrings:
                 result = token[-1].stringcache(result, result)
             return result
+
+
+        def parse_double(uni, nonmatch, next, token):
+            ''' Munged version of UCS-4 code pair stuff from
+                simplejson.
+            '''
+            ok = True
+            try:
+                uni2 = next()
+                nonmatch2 = next()
+            except:
+                ok = False
+            ok = ok and not nonmatch and uni2.startswith(r'\u') and len(uni2) == 6
+            if ok:
+                nonmatch = uni2
+                uni = 0x10000 + (((uni - 0xd800) << 10) | (int(uni2[2:], 16) - 0xdc00))
+                return uni, nonmatch2
+            token[-1].error('Invalid second ch in double sequence: %s' % repr(nonmatch), token)
 
         return parse
