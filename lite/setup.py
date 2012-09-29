92d7378fdee5affc69f4ebaca1551f2d3586a4a4
pmaupin <pmaupin@267ec534-23b7-11df-a66d-ddd16ca3c27a>
2012-09-29 22:18:16 +0000

    Updated setup.py and MANIFEST.in to shipped 0.1.0 version

--- lite/setup.py
+++ lite/setup.py
@@ -7,14 +7,14 @@ with open('README.txt') as file:
 
 setup(
     name='rsonlite',
-    version='0.1',
+    version='0.1.0',
     description='rsonlite -- extremely lightweight version of rson',
     long_description=long_description,
     author='Patrick Maupin',
     author_email='pmaupin@gmail.com',
     platforms="Independent",
     url='http://code.google.com/p/rson/',
-    packages=['rsonlite'],
+    py_modules=['rsonlite'],
     license="MIT",
     classifiers=[
         'Development Status :: 4 - Beta',
